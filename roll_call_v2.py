316fd0c4f0d671b6567bd3781a18fbf745110877
elfishmin <elfishmin@gmail.com>
2026-03-06 09:47:30 +0800

    Update roll_call_v2.py

--- roll_call_v2.py
+++ roll_call_v2.py
@@ -5,7 +5,7 @@ import requests
 import json
 import time
 
-st.set_page_config(page_title="才藝班點名系統 V34.4", page_icon="🏫", layout="wide")
+st.set_page_config(page_title="才藝班點名系統 V34.5", page_icon="🏫", layout="wide")
 
 SCRIPT_URL = "https://script.google.com/macros/s/AKfycbxTDHM3oNGMuRuKK_v8wVSM5-PWcGJfKRNMt6Sy4ClNqN280-r1oXZbRhePUD6RZ2LMVg/exec"
 
@@ -51,7 +51,7 @@ if 'unlock_confirm' not in st.session_state: st.session_state.unlock_confirm = F
 
 # --- 側邊欄 ---
 with st.sidebar:
-    st.title("🏫 點名系統 V34.4")
+    st.title("🏫 點名系統 V34.5")
     st.write(f"今天是：{current_day}")
     
     # 新增：刷新狀態按鈕
@@ -141,6 +141,7 @@ if st.session_state.current_class:
                     st.rerun()
 else:
     st.info("請選擇左側課程")
+
 
 
 
